b41aac0cf119773a50df2de316b884fc18d744c0
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-12-04 23:14:12 +0300

    app.js

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StatusBar } from 'expo-status-bar';
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+import SavedScreen from './screens/SavedScreen';
+import SettingScreen from './screens/SettingScreen';
+
+
+const Stack = createNativeStackNavigator();
+const Tab =  createBottomTabNavigator();
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-     
-      <StatusBar style="auto" />
-    </View>
+  <NavigationContainer>
+    <Tab.Navigator  style={{
+          labelStyle: { textAlign: 'center' }, // Başlıkları ortala
+        }}>
+      <Tab.Screen name='Home' component={HomeScreen} />
+      <Tab.Screen name='Map' component={MapScreen} />
+      <Tab.Screen name='Saved' component={SavedScreen} />
+      <Tab.Screen name='Setting' component={SettingScreen} />
+    </Tab.Navigator>
+  </NavigationContainer>
   );
 }
 
---
+++ screens/SettingScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function SettingScreen() {
+  return (
+    <View>
+      <Text>SettingScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
